d8fc1c671b6d774c12af5a8402ee909d25b4444c
tim vanlerberg <tim@brightsidedeveloper.com>
2024-12-22 13:41:29 -0600

    Serve

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT env variable is not set")
+	}
+
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	go func() {
+		log.Printf("Starting server on port %s\n", port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+	log.Println("Gracefully shutting down server...")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Error shutting down server: %v", err)
+	}
+
+	log.Println("Server shutdown successfully")
+}
