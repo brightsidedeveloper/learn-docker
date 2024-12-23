ab1e558c1661e526348ce11c692666707b2018cf
tim vanlerberg <tim@brightsidedeveloper.com>
2024-12-22 21:30:08 -0600

    Plz

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, Tim!"))
+		w.Write([]byte("Hello, Tim 88888"))
 	})
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
