9bee62766e4d9e0041a7c2c1c870a3d9a203d55c
tim vanlerberg <tim@brightsidedeveloper.com>
2024-12-22 19:54:16 -0600

    a

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ import (
 	"syscall"
 )
 
+/*
+
+ - name: Log in to Docker Hub
+        run: echo "${{ secrets.DOCKER_PASSWORD }}" | docker login -u "${{ secrets.DOCKER_USERNAME }}" --password-stdin
+
+      - name: Build and Push Docker Image
+        run: |
+          docker build -t ${{ secrets.DOCKER_USERNAME }}/go-server:latest .
+          docker push ${{ secrets.DOCKER_USERNAME }}/go-server:latest
+
+
+*/
+
 func main() {
 	mux := http.NewServeMux()
 
